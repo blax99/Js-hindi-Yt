9825cbc0cbb7fff0430a9b8e7a2cb1a093cbd938
Krrish <122219660+blax99@users.noreply.github.com>
2024-12-25 08:09:18 +0000

    c1

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,20 @@
+let name = "Krrish"  //String
+let age = 20         //int(numeric)
+let LoggedInfo = true     //bolean
+
+console.log(name);
+console.log(age);
+console.log(LoggedInfo);
+
+console.log(typeof LoggedInfo);   //state the datatype
+
+console.log(typeof null);
+console.log(typeof undefined);
+
+
+
+
+
+
+
+
