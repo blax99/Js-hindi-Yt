0a95ecba535d610d8b8482bc3cebcf457eb4eafd
Krrish <122219660+blax99@users.noreply.github.com>
2024-12-25 07:38:18 +0000

    C1

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 12345
+let accountEmail = "Krrish@gmail.com"
+var accountName = "Krrish"
+accountCity = "Rajbiraj"
+
+console.log(accountEmail);
+
+/*
+ Do not use var instead use let because of scope problems.
+*/
+
+
+console.table([accountId,accountEmail,accountName,accountCity])
