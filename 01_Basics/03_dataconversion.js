46e30e3c18784a9da81941ae9fef1ebeb2d504ae
Krrish <122219660+blax99@users.noreply.github.com>
2024-12-26 04:51:54 +0000

    c2

---
+++ 01_Basics/03_dataconversion.js
@@ -0,0 +1,14 @@
+let age = 10
+let str = String(age)   //conversion from number to string
+
+console.log(str);
+
+console.log(typeof str);
+
+
+let age2 = "twenty"
+let num = Number(age2)
+
+console.log(num);     // NaN-not a number
+console.log(typeof num);
+
