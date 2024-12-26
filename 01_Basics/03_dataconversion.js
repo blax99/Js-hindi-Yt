9af7a07cdf0ee542f09df8d0953434ce9046b514
Krrish <122219660+blax99@users.noreply.github.com>
2024-12-26 05:14:35 +0000

    c2

--- 01_Basics/03_dataconversion.js
+++ 01_Basics/03_dataconversion.js
@@ -1,14 +1,35 @@
 let age = 10
 let str = String(age)   //conversion from number to string
 
-console.log(str);
+//console.log(str);
 
-console.log(typeof str);
+//console.log(typeof str);
 
 
 let age2 = "twenty"
 let num = Number(age2)
 
-console.log(num);     // NaN-not a number
-console.log(typeof num);
+//console.log(num);     // NaN-not a number
+//console.log(typeof num);
+
+
+/******************OPERATIONS******************************/
+
+let str1 = "Hello"
+let str2 = " Krrish"
+let str3 = str1 + str2
+//console.log(str3);
+
+console.log(2**3);
+
+console.log(1 + "2");
+console.log(1 + 2 + "3");
+console.log("1" + 2 + 3);
+
+
+
+
+
+
+
 
