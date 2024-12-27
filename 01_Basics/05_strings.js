aee37f5890b22b5a88fd27f18e10bd50d3114c19
Krrish <122219660+blax99@users.noreply.github.com>
2024-12-27 09:41:58 +0000

    c3

---
+++ 01_Basics/05_strings.js
@@ -0,0 +1,40 @@
+/*
+const name = "Krrish"
+const repoCount = 20
+//console.log(name + repoCount + " value");
+
+//console.log(`my name is ${name} and my repo is ${repoCount}`);
+
+const gameName = new String('KrishnaCh')
+
+console.log(gameName);
+console.log(gameName.length);
+console.log(gameName.indexOf("a"));
+console.log(gameName.charAt(5));
+console.log(gameName.toUpperCase);
+
+let str = "Hello_Everyone!!"
+console.log(str.substring(0,4));
+
+*/
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+console.log(url.replace('%20','-'));
+
+console.log(url.includes('hitesh'));
+
+let str2 = "Krrish-chaudhary"
+console.log(str2.split('-'));
+
+
+
+const str1= "  Krishna    "
+console.log(str1.trim());
+
+
+
+
+
+
+
+
