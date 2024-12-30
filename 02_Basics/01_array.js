1ab608df6fc148df038b0e4d95f043e899cc24fa
Krrish <122219660+blax99@users.noreply.github.com>
2024-12-30 15:53:49 +0000

    c5

---
+++ 02_Basics/01_array.js
@@ -0,0 +1,55 @@
+// array
+
+const myArr = [0, 1, 2, 3, 4, 5]
+const myFriend = ["Anuj" ,"Shyam" ,"Ram" ]
+
+// const myArr2 = new Array(1 , 2, 3, 4)
+// console.log(myArr2[0]);
+
+
+// Array methods
+
+// myArr.push(6)
+// console.log(myArr);
+// myArr.pop()
+// console.log(myArr);
+
+// myArr.unshift(0)
+// console.log(myArr);
+
+// myArr.shift()
+// console.log(myArr);
+
+// myArr.shift()
+// console.log(myArr);
+
+
+// myFriend.push("Krishna")
+// console.log(myFriend);
+
+// const newAr = myArr.join()
+
+// console.log(myArr);
+
+// console.log(newAr);
+
+// slice, splice
+
+console.log("A ",myArr);
+
+const myn1 = myArr.slice(1, 3)
+console.log(myn1);
+
+
+
+// console.log("B ",myArr);
+
+// const myn2 = myArr.splice(1, 3)
+// console.log(myn2);
+// console.log(myArr);
+
+
+
+
+
+
