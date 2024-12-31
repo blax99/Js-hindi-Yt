e4c323a2e62c3cd6b2f6cc3f37cf227f512f67c1
Krrish <122219660+blax99@users.noreply.github.com>
2024-12-31 08:37:25 +0000

    c6

---
+++ 02_Basics/02_array.js
@@ -0,0 +1,36 @@
+const marvel_heros = ["thor" ,"hulk" ,"Ironman"]
+const dc_heros = ["flash" ,"Batman" , "superman"]
+
+// marvel_heros.push(dc_heros)
+// console.log(marvel_heros);
+
+// const all_new_heros = [...marvel_heros ,...dc_heros]   // spread operator
+// console.log(all_new_heros);
+
+
+// console.log(marvel_heros[3][2]);
+
+// let all_super_heros = marvel_heros.concat(dc_heros)
+// console.log(all_super_heros);
+
+
+// let another_array = [1 ,2 ,3 ,[4 , 5],6 ,[4 ,5 ,[5 ,6 ]]]
+
+// let real_anoher_array = another_array.flat(3)
+
+// console.log(real_anoher_array);
+
+// console.log(Array.isArray(dc_heros))
+
+// console.log(Array.from("Krishna"));
+
+// console.log(Array.from({name: "Krrish"}));
+
+// let score1 = 100
+// let score2 = 200
+// let score3 = 300
+
+// console.log(Array.of(score1,score2,score3));
+
+
+
