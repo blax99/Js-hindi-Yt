bd84358de84a8023534098ed1c5426fb202199e0
Krrish <122219660+blax99@users.noreply.github.com>
2025-01-06 16:52:34 +0000

    c10

---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,57 @@
+// const useremail = "k@krrish.ai"
+
+// if (useremail) {
+//     console.log("Got your email");
+// } else {
+//     console.log("dont have your email");
+// }
+
+// falsy values
+
+// false, 0 ,-0, BigInt 0n, "", null, undefined, NaN
+
+// truthy values
+
+// "0", " ", "false", {}, [], function() {}
+
+
+// const useremail = []
+
+// if (useremail.length === 0) {
+//     console.log("Array is empty");
+    
+// }
+
+// const userInfo = {
+//     name: "Krrish"
+// }
+
+// if ((Object.keys(userInfo)).length === 0) {
+//     console.log("object is empty");
+// }
+
+// Nullish Coalescing Operator (??) null undefined
+
+// let val
+// val = 5 ?? 10
+// console.log(val);
+
+
+// let val2
+// val2 = null ?? 10
+// console.log(val2);
+
+
+// let val3
+// val3 = undefined ?? 10
+// console.log(val3);
+
+
+// ternary operator
+
+// condition ? true : false
+
+let a = 5 , b = 10;
+
+(a > b) ? console.log("Yes") : console.log("No");
+
