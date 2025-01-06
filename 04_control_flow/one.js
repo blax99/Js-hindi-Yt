09b89a8155b38d33c13c1a84c006927da858ae0a
Krrish <122219660+blax99@users.noreply.github.com>
2025-01-06 16:14:48 +0000

    c10

---
+++ 04_control_flow/one.js
@@ -0,0 +1,59 @@
+// if
+
+// const isUserLoggedIn = true
+// if (isUserLoggedIn) {
+//     console.log("welcome user!");
+// }
+
+// if else
+
+// const temperature = 45
+// if (temperature <= 50) {
+//     console.log("temperatue is less than 50");
+// } else {
+//     console.log("temperature is greater than 50");
+// }
+
+// <, >, =>, =<, ==, !=, ===
+
+const score = 200
+ if (score > 100) {
+    let power = "fly"
+    console.log(`User power: ${power}`);
+ }
+
+//  console.log(`User power: ${power}`);  // score - limited scope
+
+// const balance = 700
+
+// if (balance < 500) {
+//     console.log("less than");
+// } 
+
+// else if (balance < 1000) {
+//     console.log("average");
+// }
+
+// else {
+//     console.log("greater than");
+// }
+
+
+// const UserLoggedIn = true
+// const debitCard = true
+
+// if (UserLoggedIn && debitCard) {
+//     console.log("allow to buy");
+// } else {
+//     console.log("not allowed");
+// }
+
+
+// const idCard = true
+// const collegeUniform = false
+
+// if (idCard || collegeUniform) {
+//     console.log("allow to enter");
+// } 
+
+
