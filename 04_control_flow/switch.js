e24d8da2d04b43ecefc4c6041ff3477e238045fe
Krrish <122219660+blax99@users.noreply.github.com>
2025-01-06 16:23:16 +0000

    c10

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,38 @@
+// switch statement
+
+// const month = 1
+
+// switch (month) {
+//     case 1:
+//         console.log("January");
+//         break;
+//     case 2:
+//         console.log("Februrary");
+//         break;
+//     case 3:
+//         console.log("March");
+//         break;
+  
+//     default:
+//         console.log("invalid entry");
+//         break;
+//     }
+
+
+const month = "Feb"
+
+switch (month) {
+    case "Jan":
+        console.log("January");
+        break;
+    case "Feb":
+        console.log("Februrary");
+        break;
+    case "Mar":
+        console.log("March");
+        break;
+  
+    default:
+        console.log("invalid entry");
+        break;
+}
