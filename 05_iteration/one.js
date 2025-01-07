bf9c84a055c59f48278e6185a5e432578f824e3f
Krrish <122219660+blax99@users.noreply.github.com>
2025-01-07 07:47:49 +0000

    c11

---
+++ 05_iteration/one.js
@@ -0,0 +1,56 @@
+// for
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`element is: ${i}`);
+// }
+
+// for (let i = 1; i <= 5; i++) {
+//     console.log(`Table for: ${i}`);
+
+//     for (let j = 1; j <= 10; j++) {
+        
+//         console.log(`${i}*${j} = ${i*j} `);
+        
+//     }
+    
+
+
+// const myArray = ["Batman", "Superman", "Spiderman"]
+
+// for (let index = 0; index < myArray.length; index++) {
+//     const element = myArray[index];
+//     console.log(element);
+    
+// }
+
+
+
+
+
+// break
+
+// for (let i = 0; i < 10; i++) {
+
+//     if (i == 5) {
+//         console.log("5 detected");
+//         break
+//     }
+
+//     console.log(`element is: ${i}`);
+
+// }
+
+
+// continue
+
+
+// for (let i = 0; i < 10; i++) {
+
+//     if (i == 5) {
+//         console.log("5 detected");
+//         continue
+//     }
+
+//     console.log(`element is: ${i}`);
+
+// }
