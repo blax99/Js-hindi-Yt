c46febbb2cfaa1b73fcc5a143025f934d721366e
Krrish <122219660+blax99@users.noreply.github.com>
2025-01-20 15:14:46 +0000

    c19

---
+++ 10_oops/object.js
@@ -0,0 +1,27 @@
+// function multiplyBy5(num) {
+//     return num * 5
+// }
+// multiplyBy5.power = 2
+
+// console.log(multiplyBy5(2));
+// console.log(multiplyBy5.power);
+// console.log(multiplyBy5.prototype);
+
+// function createUser(username, score) {
+//     this.username = username
+//     this.score = score
+// }
+
+// createUser.prototype.increment = function (){
+//     this.score++
+// }
+
+// createUser.prototype.printMe = function (){
+//     console.log(`score is: ${this.score}`);
+// }
+
+// const dataOne = new createUser('krishna', 67)
+// const dataTwo = new createUser('Anuj', 65)
+
+// dataOne.increment()
+// dataOne.printMe()
