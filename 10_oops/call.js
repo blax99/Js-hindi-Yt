e8c65d88d8db9be34fb59c12dded5ee048d094bf
Krrish <122219660+blax99@users.noreply.github.com>
2025-01-22 07:31:54 +0000

    c21

--- 10_oops/call.js
+++ 10_oops/call.js
@@ -1,13 +1,32 @@
-function assignName(username) {
-    this.username = username
-    console.log('called');
+// function assignName(username) {
+//     this.username = username
+//     console.log('called');
+// }
+
+// function user(username, email, isLoggedIn) {
+//     assignName.call(this, username)
+//     this.email = email
+//     this.isLoggedIn = isLoggedIn
+// }
+
+// const user1 = new user('krishna', 'krrish@google.com', true)
+// console.log(user1);
+
+
+// Bind in JS
+const person = {
+    fname: 'harry',
+    lname: 'kane',
+
+    getFullName: function (){
+        return `${this.fname} ${this.lname}`
+    }
 }
 
-function user(username, email, isLoggedIn) {
-    assignName.call(this, username)
-    this.email = email
-    this.isLoggedIn = isLoggedIn
+const anotherPerson = {
+    fname : 'Mary',
+    lname : 'Jane'
 }
 
-const user1 = new user('krishna', 'krrish@google.com', true)
-console.log(user1);
+const fullName = person.getFullName.bind(anotherPerson)
+console.log(fullName());
