f9a0d804d9813e973e2e57d608efc298a13afbc0
blax99 <chaudhariikrishna1@gmail.com>
2025-03-11 14:54:41 +0545

    tic-tac-toe added

---
+++ 07_Projects/Tic Tac Toe/tic.js
@@ -0,0 +1,30 @@
+let arr = Array(9).fill(null)
+let currentPlayer = 'X';
+function checkWinner(){
+    if(
+        (arr[0] !== null && arr[0] == arr[1] && arr[1] == arr[2]) ||
+        (arr[3] !== null && arr[3] == arr[4] && arr[4] == arr[5]) ||
+        (arr[6] !== null && arr[6] == arr[7] && arr[7] == arr[8]) ||
+        (arr[0] !== null && arr[0] == arr[3] && arr[3] == arr[8]) ||
+        (arr[1] !== null && arr[1] == arr[4] && arr[4] == arr[7]) ||
+        (arr[2] !== null && arr[2] == arr[5] && arr[5] == arr[8]) ||
+        (arr[0] !== null && arr[0] == arr[4] && arr[4] == arr[8]) ||
+        (arr[2] !== null && arr[2] == arr[4] && arr[4] == arr[6])
+    ){
+        document.write(`Player ${currentPlayer} Wins`);
+    }else{
+        if(!arr.some(e => e === null)){
+            document.write('Game Draw!');
+            return;
+        }
+    }
+}
+
+function handleClick(e){
+    const id = Number(e.id);
+    if (arr[id] !== null) return
+    arr[id] = currentPlayer;
+    e.innerText = currentPlayer;
+    checkWinner();    
+    currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+}
